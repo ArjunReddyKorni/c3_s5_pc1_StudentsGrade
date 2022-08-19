497422c7ca280162022c6bd3fc4d7c0f5fe1f1ff
Korni Arjun Reddy <arjunreddykorni@gmail.com>
2022-08-19 21:47:15 +0530

    implemented as per the Question.

---
+++ src/com/marks/StudentMarks.java
@@ -0,0 +1,20 @@
+package com.marks;
+
+public class StudentMarks {
+
+    public int calculateMarks(int math,int physics,int chemistry,int biology){
+        int sum =0;
+        return sum = math + physics + chemistry + biology;
+    }
+
+    public int calculateMarks(int math,int physics,int chemistry){
+        int sum =0;
+        return sum = math + physics + chemistry ;
+    }
+
+    public double calculateMarks(double businessStudies,double finance,double accounting){
+        double sum = 0;
+        return sum = businessStudies + finance + accounting;
+    }
+
+}
---
+++ src/com/marks/StudentMarksImpl.java
@@ -0,0 +1,13 @@
+package com.marks;
+
+public class StudentMarksImpl {
+
+    public static void main(String[] args) {
+
+        StudentMarks studentMarks = new StudentMarks();
+
+        System.out.println("studentMarks of Medical Studies = " + studentMarks.calculateMarks(97, 99, 78));
+        System.out.println("studentMarks of Non medical Studies = " + studentMarks.calculateMarks(99, 89, 89, 89));
+        System.out.println("studentMarks of Businness Studies = " + studentMarks.calculateMarks(24.5, 99, 99.5));
+    }
+}
